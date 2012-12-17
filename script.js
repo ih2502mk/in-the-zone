e62fa8110425b09350369b6bc0a708aa2f61ab4d
Ihor Marusyk <ih2502mk@gmail.com>
2012-12-17 17:55:11 +0200

    ctrl

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 function TimeZoneController($scope) {
+	var 
+		  TIME_FORMAT_24H = "24h"
+		, TIME_FORMAT_12H = "12h"
+
+
+
 	$scope.timeZones = [
 		{
 			"utc-offset" : -11,
 			"active" : false,
 			"current" : false,
-			"timeFormat" : "24h" // 24h or "12h"
+			"timeFormat" : TIME_FORMAT_24H
 		},
 		{
 			"utc-offset" : -10,
 			"active" : false,
 			"current" : false,
-			"timeFormat" : "24h"
-		}
+			"timeFormat" : TIME_FORMAT_24H
+		},
+		{
+			"utc-offset" : -9,
+			"active" : false,
+			"current" : false,
+			"timeFormat" : TIME_FORMAT_24H
+		},
+
 	];
 
 	$scope.range = {
@@ -20,7 +33,33 @@ function TimeZoneController($scope) {
 	};
 
 	$scope.valueToTime = function(value, format) {
-		format = format || "24h";
-		
+		format = format || TIME_FORMAT_24H;
+		   
+    var hrs = parseInt(Math.floor(value / 2))
+      , mins = (value % 2) ? "30" : "00",
+      , a = false;
+    
+    hrs = (hrs < 0) ? 24 + hrs : hrs;
+    hrs = (hrs > 24) ? hrs - 24 : hrs;
+
+    if (format === TIME_FORMAT_12H) {
+    	if (hrs > 12 ) {
+    		hrs = hrs - 12;
+    		a = " PM";
+    	}
+    	else if (hrs < 12) {
+    		a = " AM"
+    	}
+    	else if ( hrs === 12 && mins == "00" ) {
+    		a = " PM";	
+    	}
+    	else if ( hrs === 0 && mins == "00" ) {
+    		hrs = 12;
+    		a = " AM";	
+    	}
+    }
+
+    
+    return hrs + ":" + mins + a;   
 	}
 }

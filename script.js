f2693f24aa38bfb953c93013af18e8779ab53b9d
Ihor Marusyk <ih2502mk@gmail.com>
2012-12-17 18:27:57 +0200

    modularizing

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+angular.module("TimeFormatsModule", []).
+	filter('value48ToTime', function() {
+		return function(value, format) {
+			format = format || TIME_FORMAT_24H;
+		   
+	    var hrs = parseInt(Math.floor(value / 2))
+	      , mins = (value % 2) ? "30" : "00"
+	      , a = false;
+	    
+	    hrs = (hrs < 0) ? 24 + hrs : hrs;
+	    hrs = (hrs > 24) ? hrs - 24 : hrs;
+
+	    if (format === TIME_FORMAT_12H) {
+	    	if (hrs > 12 ) {
+	    		hrs = hrs - 12;
+	    		a = " PM";
+	    	}
+	    	else if (hrs < 12) {
+	    		a = " AM"
+	    	}
+	    	else if ( hrs === 12 && mins == "00" ) {
+	    		a = " PM";	
+	    	}
+	    	else if ( hrs === 0 && mins == "00" ) {
+	    		hrs = 12;
+	    		a = " AM";	
+	    	}
+	    }
+
+	    
+	    return hrs + ":" + mins + a;   
+		}
+	})
+
 function TimeZoneController($scope) {
 	var 
 		  TIME_FORMAT_24H = "24h"
@@ -28,38 +62,7 @@ function TimeZoneController($scope) {
 	];
 
 	$scope.range = {
-		"topValue" : 35
+		"topValue" : 35,
 		"bottomValue" : 12
 	};
-
-	$scope.valueToTime = function(value, format) {
-		format = format || TIME_FORMAT_24H;
-		   
-    var hrs = parseInt(Math.floor(value / 2))
-      , mins = (value % 2) ? "30" : "00",
-      , a = false;
-    
-    hrs = (hrs < 0) ? 24 + hrs : hrs;
-    hrs = (hrs > 24) ? hrs - 24 : hrs;
-
-    if (format === TIME_FORMAT_12H) {
-    	if (hrs > 12 ) {
-    		hrs = hrs - 12;
-    		a = " PM";
-    	}
-    	else if (hrs < 12) {
-    		a = " AM"
-    	}
-    	else if ( hrs === 12 && mins == "00" ) {
-    		a = " PM";	
-    	}
-    	else if ( hrs === 0 && mins == "00" ) {
-    		hrs = 12;
-    		a = " AM";	
-    	}
-    }
-
-    
-    return hrs + ":" + mins + a;   
-	}
 }

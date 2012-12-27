37af4369c0c4e47de1ac8556012272dafced3196
Ihor Marusyk <ih2502mk@gmail.com>
2012-12-28 01:57:00 +0200

    experimenting and creating a directive

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ angular.module("TimeFormatsModule", [])
 	
 	.filter('value48ToTime', function() {
 		return function(value, format) {
+
 			format = format || "24h";
 		   
 	    var hrs = parseInt(Math.floor(value / 2))
@@ -36,7 +37,57 @@ angular.module("TimeFormatsModule", [])
 		return function(value) {
 			return value ? "active" : "";
 		}
-	});
+	})
+
+	.directive('rangeSlider', function factory(){
+		return {
+			template : 
+					'<div class="slider" >' 
+				+ '<div class="range"></div>'
+				+ '<a href="#" class="slider-handle top" ></a>'
+				+ '<a href="#" class="slider-handle bottom" ></a>'
+				+ '</div>',
+			replace : true,
+			restrict : "AEC",
+			scope : {
+				topValue : "=",
+				bottomValue : "=",
+				min : "@",
+				max : "@",
+				step : "@",
+				onChange : "&"
+			},
+			link : function(scope, element, attrs) {
+				var range = element.find('div')
+					,	topHandle = element.find('a')[0]
+					, bottomHandle = element.find('a')[1] ;
+
+
+			}
+		};
+	})	
+
+	//an experimentation thing
+	.directive('fooDir', function factory(){
+		return {
+			template : 
+					'<div style="position: absolute; z-index: 1000;" >{{passedValue}}<br /><br /></div>',
+			replace : true,
+			restrict : "AEC",
+			scope : {
+				passedValue : "=",
+				onAction : "&"		
+			},
+			link: function(scope, element, attrs) {
+				element.bind('click', function(){
+					scope.onAction({arg1 : 233, arg2 : "HELLO!"});	
+				})
+				console.log(element.find('br'));
+			}
+		};
+	})
+
+	;
 
 function TimeZoneController($scope) {
 
@@ -51,7 +102,7 @@ function TimeZoneController($scope) {
 			"utcOffset" : -10,
 			"active" : false,
 			"current" : false,
-			"timeFormat" : "24h"
+			"timeFormat" : "12h"
 		},
 		{
 			"utcOffset" : -9,
@@ -67,5 +118,9 @@ function TimeZoneController($scope) {
 		"bottomValue" : 12
 	};
 
-	$scope.w = "25%";
+	$scope.aaa = "Hello man!!!";
+
+	$scope.log = function() {
+		console.log(arguments);
+	}
 }

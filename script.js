0d2520ae1be8655d4dccf507463df1741274c53a
Ihor Marusyk <ih2502mk@gmail.com>
2013-01-08 16:56:59 +0200

    draggable box

--- script.js
+++ script.js
@@ -84,24 +84,49 @@ angular.module("TimeFormatsModule", [])
 				});
 
 				scope.moving = false;
-				scope.startX = 0;
-				scope.startY = 0;
+				
+				scope.currentX = 0;
+				scope.currentY = 0;
+				scope.oldX = 0;
+				scope.oldY = 0;
 
 				element.bind('mousedown', function(event){
 					scope.moving = true;
-					scope.startX = event.pageX;
-					scope.startY = event.pageY;
+					scope.currentX = parseFloat(element.css("left")) || 0;
+					scope.currentY = parseFloat(element.css("top")) || 0;
+					scope.oldX = event.pageX;
+					scope.oldY = event.pageY;
+
+					$document[0].body.style.webkitUserSelect = "none";
+					$document[0].body.style.MozUserSelect = "none";
+					$document[0].body.style.userSelect = "none";
 				});
+
 				element.bind('mouseup', function() {
 					scope.moving = false;
+					
+					$document[0].body.style.webkitUserSelect = "all";
+					$document[0].body.style.MozUserSelect = "all";
+					$document[0].body.style.userSelect = "all";
 				});
 				
 				$document.bind('mousemove', function(event){
 					if (scope.moving) {
+						var offsetX = event.pageX - scope.oldX
+							, offsetY = event.pageY - scope.oldY;
+						
 						element.css({
-							"top" : event.pageY + "px",
-							"left" : event.pageX + "px"
-						});						
+							"top" : scope.currentY + offsetY + "px",
+							"left" : scope.currentX + offsetX + "px"
+						});
+
+						scope.oldX = event.pageX;
+						scope.oldY = event.pageY;
+
+						scope.currentX = scope.currentX + offsetX;
+						scope.currentY = scope.currentY + offsetY;
+
+						return false;
 					}
 				});
 

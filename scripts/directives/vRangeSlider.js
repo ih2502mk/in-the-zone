e3ee5c09547cf01c8d2c1325792830d8750e9b34
Ihor Marusyk <ih2502mk@gmail.com>
2013-01-20 15:14:36 +0200

    using compile

--- scripts/directives/vRangeSlider.js
+++ scripts/directives/vRangeSlider.js
@@ -27,167 +27,171 @@ angular.module('VerticalRangeSlider', [])
 				min : "@",
 				max : "@"
 			},
-			link : function(scope, element, attrs) {
-				var range = element.find('div')
-					,	topHandle = angular.element(element.find('a')[0])
-					, bottomHandle = angular.element(element.find('a')[1])					 
-					;
-
-				element.css({
-					"position": "relative",
-					"width" : "100%",
-					"height" : "100%"
-				});
-				var elementHeight = element[0].clientHeight;
-
-				topHandle.css({
-					"display" : "block",
-					"position" : "absolute",
-					"width" : "100%",
-					"height" : "5px",
-					"backgroundColor" : "#4a757d",
-					"top" : 0
-				});
-				var topHandleHeight = topHandle[0].clientHeight;
-
-				bottomHandle.css({
-					"display" : "block",
-					"position" : "absolute",
-					"width" : "100%",
-					"height" : "5px",
-					"backgroundColor" : "#00757d",
-					"top" : (elementHeight - 5) + "px"  
-				});
-				var bottomHandleHeight = bottomHandle[0].clientHeight;
-
-				range.css({
-					"position" : "absolute",
-					"width" : "100%",
-					"backgroundColor" : "#31c8e3",
-					"top" : topHandle[0].offsetTop + 3 + "px",
-					"bottom" : elementHeight - bottomHandle[0].offsetTop - 3 + "px"
-				});
-
-				attrs.min = parseInt(attrs.min) || 0;
-				attrs.max = parseInt(attrs.max) || (elementHeight 
-					- topHandleHeight - bottomHandleHeight);
-
-				scope.topValue = scope.topValue || attrs.min;
-				scope.bottomValue = scope.bottomValue || attrs.max;
-
-				var pxStep = ( elementHeight 
-						- topHandleHeight 
-						- bottomHandleHeight ) / (attrs.max - attrs.min);
+			compile : function (element, attrs) {
 				
-				var topMoving = false
-					, topCurrentY = topHandle[0].offsetTop || 0
-					, topOldY = 0
-					, bottomMoving = false
-					, bottomCurrentY = topHandle[0].offsetTop || 0
-					, bottomOldY = 0
-					;
+				
+				return function link(scope, element, attrs) {
+					var range = element.find('div')
+						,	topHandle = angular.element(element.find('a')[0])
+						, bottomHandle = angular.element(element.find('a')[1])					 
+						;
 
-				topHandle.bind('mousedown', function(event){
+					element.css({
+						"position": "relative",
+						"width" : "100%",
+						"height" : "100%"
+					});
+					var elementHeight = element[0].clientHeight;
 
-					topMoving = true;
-					topCurrentY = topHandle[0].offsetTop || 0;
-					topOldY = event.pageY;
+					topHandle.css({
+						"display" : "block",
+						"position" : "absolute",
+						"width" : "100%",
+						"height" : "5px",
+						"backgroundColor" : "#4a757d",
+						"top" : 0
+					});
+					var topHandleHeight = topHandle[0].clientHeight;
 
-					disableUserSelect();
-				});
+					bottomHandle.css({
+						"display" : "block",
+						"position" : "absolute",
+						"width" : "100%",
+						"height" : "5px",
+						"backgroundColor" : "#00757d",
+						"top" : (elementHeight - 5) + "px"  
+					});
+					var bottomHandleHeight = bottomHandle[0].clientHeight;
 
-				bottomHandle.bind('mousedown', function(event){
+					range.css({
+						"position" : "absolute",
+						"width" : "100%",
+						"backgroundColor" : "#31c8e3",
+						"top" : topHandle[0].offsetTop + 3 + "px",
+						"bottom" : elementHeight - bottomHandle[0].offsetTop - 3 + "px"
+					});
 
-					bottomMoving = true;
-					bottomCurrentY = bottomHandle[0].offsetTop || 0;
-					bottomOldY = event.pageY;
+					attrs.min = parseInt(attrs.min) || 0;
+					attrs.max = parseInt(attrs.max) || (elementHeight 
+						- topHandleHeight - bottomHandleHeight);
 
-					disableUserSelect();
-				});
+					scope.topValue = scope.topValue || attrs.min;
+					scope.bottomValue = scope.bottomValue || attrs.max;
 
-				$document.bind('mouseup', function(){
-					topMoving = false;
-					bottomMoving = false;
+					var pxStep = ( elementHeight 
+							- topHandleHeight 
+							- bottomHandleHeight ) / (attrs.max - attrs.min);
 					
-					enableUserSelect();					
-				});
+					var topMoving = false
+						, topCurrentY = topHandle[0].offsetTop || 0
+						, topOldY = 0
+						, bottomMoving = false
+						, bottomCurrentY = topHandle[0].offsetTop || 0
+						, bottomOldY = 0
+						;
 
-				$document.bind('mousemove', function(event){
-					var offsetY
-						, top;
+					topHandle.bind('mousedown', function(event){
 
-					if (topMoving) {
-						offsetY = event.pageY - topOldY;
-
-						top = topCurrentY + offsetY;
-						if ( top >= 0 && top <= bottomHandle[0].offsetTop - topHandleHeight) { 
-
-							scope.topValue = Math.round(top / pxStep);
-							scope.$apply();
-						}				
-					
+						topMoving = true;
+						topCurrentY = topHandle[0].offsetTop || 0;
 						topOldY = event.pageY;
 
-						topCurrentY = topCurrentY + offsetY;
+						disableUserSelect();
+					});
 
-						return false;
-					}
+					bottomHandle.bind('mousedown', function(event){
 
-					if (bottomMoving) {
-						offsetY = event.pageY - bottomOldY;
-
-						top = bottomCurrentY + offsetY;
-						if ( top >= topHandle[0].offsetTop + topHandleHeight 
-							&& top <= elementHeight - bottomHandle[0].clientHeight) { 
-							
-							scope.bottomValue = Math.round(top / pxStep);
-							scope.$apply();
-						}
-						
+						bottomMoving = true;
+						bottomCurrentY = bottomHandle[0].offsetTop || 0;
 						bottomOldY = event.pageY;
 
-						bottomCurrentY = bottomCurrentY + offsetY;
-
-						return false;
-					}
-
-				});
-
-				scope.$watch('topValue', function(newValue, oldValue) {
-					newValue = !isNaN(newValue) ? parseInt(newValue) : oldValue;
-					newValue = newValue <= attrs.min ? attrs.min : newValue;
-					newValue = newValue >= scope.bottomValue ? scope.bottomValue : newValue;
-
-					var top = Math.round (pxStep * newValue);
-									
-					topHandle.css({
-						"top" : top + "px"
+						disableUserSelect();
 					});
 
-					range.css({
-						"top" : top + 3 + "px"
+					$document.bind('mouseup', function(){
+						topMoving = false;
+						bottomMoving = false;
+						
+						enableUserSelect();					
 					});
-					
-				});
 
-				scope.$watch('bottomValue', function(newValue, oldValue) {
-					newValue = !isNaN(newValue) ? parseInt(newValue) : oldValue;
-					newValue = newValue <= scope.topValue ? scope.topValue : newValue;
-					newValue = newValue >= attrs.max ? attrs.max : newValue;
-					
-					var top = Math.round (pxStep * newValue) 
-							+ topHandleHeight;
+					$document.bind('mousemove', function(event){
+						var offsetY
+							, top;
+
+						if (topMoving) {
+							offsetY = event.pageY - topOldY;
+
+							top = topCurrentY + offsetY;
+							if ( top >= 0 && top <= bottomHandle[0].offsetTop - topHandleHeight) { 
+
+								scope.topValue = Math.round(top / pxStep);
+								scope.$apply();
+							}				
+						
+							topOldY = event.pageY;
+
+							topCurrentY = topCurrentY + offsetY;
+
+							return false;
+						}
+
+						if (bottomMoving) {
+							offsetY = event.pageY - bottomOldY;
+
+							top = bottomCurrentY + offsetY;
+							if ( top >= topHandle[0].offsetTop + topHandleHeight 
+								&& top <= elementHeight - bottomHandle[0].clientHeight) { 
+								
+								scope.bottomValue = Math.round(top / pxStep);
+								scope.$apply();
+							}
+							
+							bottomOldY = event.pageY;
+
+							bottomCurrentY = bottomCurrentY + offsetY;
+
+							return false;
+						}
+
+					});
+
+					scope.$watch('topValue', function(newValue, oldValue) {
+						newValue = !isNaN(newValue) ? parseInt(newValue) : oldValue;
+						newValue = newValue <= attrs.min ? attrs.min : newValue;
+						newValue = newValue >= scope.bottomValue ? scope.bottomValue : newValue;
+
+						var top = Math.round (pxStep * newValue);
 										
-					bottomHandle.css({
-						"top" : top + "px"								
+						topHandle.css({
+							"top" : top + "px"
+						});
+
+						range.css({
+							"top" : top + 3 + "px"
+						});
+						
 					});
 
-					range.css({
-						"bottom" : elementHeight - top - 3 + "px"
+					scope.$watch('bottomValue', function(newValue, oldValue) {
+						newValue = !isNaN(newValue) ? parseInt(newValue) : oldValue;
+						newValue = newValue <= scope.topValue ? scope.topValue : newValue;
+						newValue = newValue >= attrs.max ? attrs.max : newValue;
+						
+						var top = Math.round (pxStep * newValue) 
+								+ topHandleHeight;
+											
+						bottomHandle.css({
+							"top" : top + "px"								
+						});
+
+						range.css({
+							"bottom" : elementHeight - top - 3 + "px"
+						});
+
 					});
 
-				});
-
+				} 
 			}
 		};
 	})

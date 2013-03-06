3243548a8eeb37f408a8c7b4f7bb378f2785dd1d
Ihor Marusyk <ih2502mk@gmail.com>
2013-03-06 23:01:01 +0200

    let's make it watchable

--- scripts/services/geolocationService.js
+++ scripts/services/geolocationService.js
@@ -1,15 +1,19 @@
 angular.module('InTheZone')
 	.service('geolocationService', function($window){
 		
+		var pos = {
+			lat: false,
+			lng: false
+		};
+
 		return {
 			getCurrentPosition : function(cb) {
 				if($window.navigator.geolocation) {
 		      $window.navigator.geolocation.getCurrentPosition(
 		      	function(position) {
-		        	cb(false, {
-		        		lat: position.coords.latitude,
-		        		lng: position.coords.longitude
-		        	});
+		      		pos.lat = position.coords.latitude;
+		      		pos.lng = position.coords.longitude;
+		        	cb(false, pos);
 		      	}, 
 		      	function() {
 		      		cb(true);
